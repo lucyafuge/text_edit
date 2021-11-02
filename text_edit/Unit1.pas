3a81b3bf8484d86035c4e156635ffa3668354084
lucyafuge <74184134+lucyafuge@users.noreply.github.com>
2021-11-02 15:40:38 +0300

    Add files via upload

--- text_edit/Unit1.pas
+++ text_edit/Unit1.pas
@@ -8,6 +8,10 @@ type
   Form1 = class(Form)
     procedure toolStripMenuItem_open_Click(sender: Object; e: EventArgs);
     procedure toolStripMenuItem_save_Click(sender: Object; e: EventArgs);
+    procedure toolStripMenuItem3_Click(sender: Object; e: EventArgs);
+    procedure toolStripMenuItem4_Click(sender: Object; e: EventArgs);
+    procedure toolStripMenuItem5_Click(sender: Object; e: EventArgs);
+    procedure toolStripMenuItem6_Click(sender: Object; e: EventArgs);
   {$region FormDesigner}
   internal
     {$resource Unit1.Form1.resources}
@@ -17,6 +21,11 @@ type
     toolStripMenuItem_save: ToolStripMenuItem;
     openFileDialog: OpenFileDialog;
     MainText: RichTextBox;
+    toolStripMenuItem1: ToolStripMenuItem;
+    toolStripMenuItem3: ToolStripMenuItem;
+    toolStripMenuItem4: ToolStripMenuItem;
+    toolStripMenuItem5: ToolStripMenuItem;
+    toolStripMenuItem6: ToolStripMenuItem;
     saveFileDialog: SaveFileDialog;
     {$include Unit1.Form1.inc}
   {$endregion FormDesigner}
@@ -52,6 +61,26 @@ begin
   saveFileDialog.ShowDialog;
   ///Сохраняем файл
   MainText.SaveFile(saveFileDialog.FileName, RichTextBoxStreamType.PlainText)
+end;
+
+procedure Form1.toolStripMenuItem3_Click(sender: Object; e: EventArgs);
+begin
+  MainText.Copy;
+end;
+
+procedure Form1.toolStripMenuItem4_Click(sender: Object; e: EventArgs);
+begin
+    MainText.Cut;
+end;
+
+procedure Form1.toolStripMenuItem5_Click(sender: Object; e: EventArgs);
+begin
+    MainText.Paste;
+end;
+
+procedure Form1.toolStripMenuItem6_Click(sender: Object; e: EventArgs);
+begin
+    MainText.SelectedText := '';
 end;
 
 end.
